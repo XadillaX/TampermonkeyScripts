c768a15741a13ed025d8c245e2d078ee367402ab
XadillaX <admin@xcoder.in>
2014-12-17 15:56:31 +0800

    udpate gitlab

--- gitlab-fonter.js
+++ gitlab-fonter.js
@@ -3,11 +3,11 @@
 // @updateURL    https://raw.githubusercontent.com/XadillaX/TampermonkeyScripts/master/gitlab-fonter.js
 // @downloadURL  https://raw.githubusercontent.com/XadillaX/TampermonkeyScripts/master/gitlab-fonter.js
 // @namespace    http://xcoder.in/
-// @version      0.3
+// @version      0.4
 // @description  Change gitlab font to "Fantasque Sans Mono". You may add your own gitlab address your company hosted.
 // @author       XadillaX
 // @match        http://gitlab.com/*
 // ==/UserScript==
 
-GM_addStyle(".code { font-family: \"fantasque sans mono\"; font-size: 14px; } pre code { font-size: 14px; }");
+GM_addStyle("code, kbd, pre, samp { font-family: \"fantasque sans mono\"; font-size: 14px; } pre code { font-size: 14px; }");
 
