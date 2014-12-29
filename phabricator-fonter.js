6dcbdc24f0ad1a8b742d2c6ba84578e900e5e84c
XadillaX <admin@xcoder.in>
2014-12-29 21:14:52 +0800

    phabricator fonter

---
+++ phabricator-fonter.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Phabricator code font to "Fantasque Sans Mono"
+// @updateURL    https://raw.githubusercontent.com/XadillaX/TampermonkeyScripts/master/phabricator-fonter.js
+// @downloadURL  https://raw.githubusercontent.com/XadillaX/TampermonkeyScripts/master/phabricator-fonter.js
+// @namespace    http://xcoder.in/
+// @version      0.1
+// @description  Change phabricator font to "Fantasque Sans Mono".
+// @author       XadillaX
+// @match        http://phabricator.huaban.com/*
+// ==/UserScript==
+
+GM_addStyle(".PhabricatorMonospaced, .phabricator-remarkup .remarkup-code-block .remarkup-code { font-family: \"Fantasque Sans Mono\"; font-size: 13px; }");
+
