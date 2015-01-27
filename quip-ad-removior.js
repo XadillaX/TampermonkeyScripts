1a761adb2469ebd47a052e11b52e40ec7cdfb03b
XadillaX <admin@xcoder.in>
2015-01-27 16:24:43 +0800

    quip-ad-removior

---
+++ quip-ad-removior.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Quip AD Removior
+// @updateURL    https://raw.githubusercontent.com/XadillaX/TampermonkeyScripts/master/quip-ad-removior.js
+// @downloadURL  https://raw.githubusercontent.com/XadillaX/TampermonkeyScripts/master/quip-ad-removior.js
+// @namespace    http://xcoder.in/
+// @version      0.1
+// @description  Remove AD of quip.
+// @author       XadillaX
+// @match        https://quip.com/*
+// ==/UserScript==
+
+GM_addStyle("#nag-bar { display: none; } #app { top: 0; }");
+GM_addStyle("body.nag-bar > #app { top: 0; }");
+
