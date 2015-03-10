00079b7b025b6f4cc8b40278936a46b8f2e98475
Flandre Scarlet <i@2333.moe>
2015-03-10 18:24:11 +0800

    Update quip-ad-removior.js

--- quip-ad-removior.js
+++ quip-ad-removior.js
@@ -3,12 +3,11 @@
 // @updateURL    https://raw.githubusercontent.com/XadillaX/TampermonkeyScripts/master/quip-ad-removior.js
 // @downloadURL  https://raw.githubusercontent.com/XadillaX/TampermonkeyScripts/master/quip-ad-removior.js
 // @namespace    http://xcoder.in/
-// @version      0.1
+// @version      0.2
 // @description  Remove AD of quip.
 // @author       XadillaX
 // @match        https://quip.com/*
 // ==/UserScript==
 
-GM_addStyle("#nag-bar { display: none; } #app { top: 0; }");
+GM_addStyle("#nag-bar { display: none; } body.has-nag-bar #app { top: 0; }");
 GM_addStyle("body.nag-bar > #app { top: 0; }");
-
